ba811207e5e710b55059eb779c1eb6ed28127c25
Dmeowmixer <ransonikuo@gmail.com>
2019-01-16 14:28:35 -1000

    basic html in place, targeted w/ javascript(file loading)

---
+++ app.js
@@ -0,0 +1,9 @@
+let button = document.getElementById('submitButton');
+let age = document.getElementById('ageInput').value;
+let restingHR = document.getElementById('restingHRInput').value;
+console.log(age,restingHR);
+
+
+button.addEventListener('click', function(){
+  
+});
